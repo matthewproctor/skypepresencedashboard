e21f32e5d4c93c8e34833e56f16d6bf7d8bb970a
Matthew Proctor <matthew@proctor.com.au>
2015-09-27 10:32:10 +1000

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,41 @@
+/* 
+ *
+ * Matthew Proctor's Presence Dashboard
+ * Functions to expand and collapse the left-side navbar
+ * 
+ */
+
+(function () {
+    $(function () {
+        var collapseMyMenu, expandMyMenu, hideMenuTexts, showMenuTexts;
+        expandMyMenu = function () {
+            return $("nav.sidebar").removeClass("sidebar-menu-collapsed").addClass("sidebar-menu-expanded");
+        };
+        collapseMyMenu = function () {
+            return $("nav.sidebar").removeClass("sidebar-menu-expanded").addClass("sidebar-menu-collapsed");
+        };
+        showMenuTexts = function () {
+            return $("nav.sidebar ul a span.expanded-element").show();
+        };
+        hideMenuTexts = function () {
+            return $("nav.sidebar ul a span.expanded-element").hide();
+        };
+        return $("#justify-icon").click(function (e) {
+            if ($(this).parent("nav.sidebar").hasClass("sidebar-menu-collapsed")) {
+                expandMyMenu();
+                showMenuTexts();
+                $(this).css({
+                    color: "#000"
+                });
+            } else if ($(this).parent("nav.sidebar").hasClass("sidebar-menu-expanded")) {
+                collapseMyMenu();
+                hideMenuTexts();
+                $(this).css({
+                    color: "#FFF"
+                });
+            }
+            return false;
+        });
+    });
+
+}).call(this);
